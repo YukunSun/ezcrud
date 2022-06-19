95e09c6e3f43eb20209080d96a5ba01fbf973115
sunyk <sunyukun@shopin.cn>
2022-06-19 17:59:57 +0800

    init

---
+++ ez-providers/ez-provider-json/src/main/java/com/sunyukun/Main.java
@@ -0,0 +1,12 @@
+
+package com.sunyukun;
+/**
+ * @author: sun.yukun@foxmail.com
+ * Time: ${DATE} ${TIME}
+ * Blog: sunyukun.com
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
